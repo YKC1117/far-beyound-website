e61792483bdf6cdb724f3ead97d8d30fecf109c9
可樂 <108776285+YKC1117@users.noreply.github.com>
2026-09-11 09:52:00 +0800

    Add social link data placeholder

---
+++ assets/js/site-social.js
@@ -0,0 +1,6 @@
+// Social links discovered from the current official Far-beyound website.
+// The importer updates this file when an official public link can be confirmed.
+window.FBSocialLinks = {
+  generatedAt: '2026-09-11',
+  youtube: ''
+};
