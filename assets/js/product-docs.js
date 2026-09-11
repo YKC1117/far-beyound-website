c2a05a48bcbc820c481fa394e85e3ecbea71a5f7
可樂 <108776285+YKC1117@users.noreply.github.com>
2026-09-11 09:51:52 +0800

    Seed official product document links

---
+++ assets/js/product-docs.js
@@ -0,0 +1,17 @@
+// Product document links imported from the current official Far-beyound website.
+// This file is refreshed by import-product-docs.yml.
+window.FBProductDocs = {
+  generatedAt: '2026-09-11',
+  source: 'https://www.far-beyound.com.tw',
+  items: {
+    'https://www.far-beyound.com.tw/product/1/5/11/6': [
+      { label: '產品型錄', type: 'PDF', url: 'https://www.zebra.com/content/dam/zebra_dam/zh_cn/spec-sheets/zt600-series-specification-sheet-zh-cn.pdf' },
+      { label: '使用手冊', type: 'PDF', url: 'https://www.zebra.com/content/dam/support-dam/zh_tw/documentation/unrestricted/guide/product/zt610-zt620-ug-colortouch-zhtw.pdf' }
+    ],
+    'https://www.far-beyound.com.tw/product/1/5/12/5': [
+      { label: '產品型錄', type: 'PDF', url: 'https://www.zebra.com/content/dam/zebra_dam/zh_cn/spec-sheets/zt400-series-spec-sheet-zh-cn.pdf' },
+      { label: '使用手冊', type: 'PDF', url: 'https://www.zebra.com/content/dam/support-dam/zh_tw/documentation/unrestricted/guide/product/zt411-zt421-ug-zhtw.pdf' },
+      { label: '快速參考指南', type: 'PDF', url: 'https://www.zebra.com/content/dam/zebra_new_ia/en-us/manuals/printers/industrial/zt400/zt411-zt421-sg-ww.pdf' }
+    ]
+  }
+};
